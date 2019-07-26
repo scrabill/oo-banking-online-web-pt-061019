fb1d507bd61e38070e53b02fc271eb2ac3f09d27
Shannon V. Crabill <shannon.crabill@gmail.com>
2019-07-26 22:57:41 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -30,7 +30,10 @@ class BankAccount
   end
 
   def valid?
+
     self.balance > 0 && self.status == "open"
+    
+    # if self.balance > 0 && self.status == "open"
     #   # binding.pry
     #   true
     # else
